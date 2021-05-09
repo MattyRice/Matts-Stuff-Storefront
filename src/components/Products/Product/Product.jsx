4c56d174a6d4b41c58f0775bbb40b58d78e0cec5
Matt <markricio@gmail.com>
2021-05-09 17:06:59 -0400

    CardActionArea Added. Todo: Create product page

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -9,6 +9,7 @@ import {
   CardActionArea,
 } from "@material-ui/core";
 import { Add, AddShoppingCartRounded } from "@material-ui/icons";
+import { Link } from "react-router-dom";
 
 import useStyles from "./styles";
 
@@ -18,26 +19,32 @@ const Product = ({ product, onAddToCart }) => {
   return (
     <CardActionArea>
       <Card className={classes.root}>
-        <CardMedia
-          className={classes.media}
-          image={product.media.source}
-          title={product.name}
-        />
-        <CardContent>
-          <div className={classes.cardContent}>
-            <Typography variant="h5" gutterBottom>
-              {product.name}
-            </Typography>
-            <Typography variant="h5" gutterBottom>
-              {product.price.formatted_with_symbol}
-            </Typography>
-          </div>
-          <Typography
-            dangerouslySetInnerHTML={{ __html: product.description }}
-            variant="body2"
-            color="textSecondary"
+        <Link
+          to={"product-view/${product.id}"}
+          className={classes.link}
+          style={{ textDecoration: "none" }}
+        >
+          <CardMedia
+            className={classes.media}
+            image={product.media.source}
+            title={product.name}
           />
-        </CardContent>
+          <CardContent>
+            <div className={classes.cardContent}>
+              <Typography variant="h5" gutterBottom>
+                {product.name}
+              </Typography>
+              <Typography variant="h5" gutterBottom>
+                {product.price.formatted_with_symbol}
+              </Typography>
+            </div>
+            <Typography
+              dangerouslySetInnerHTML={{ __html: product.description }}
+              variant="body2"
+              color="textSecondary"
+            />
+          </CardContent>
+        </Link>
         <CardActions disableSpacing className={classes.cardActions}>
           <IconButton aria-label="Add to Cart" onClick={onAddToCart}>
             <AddShoppingCartRounded />
