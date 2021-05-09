9e02adec9c4d7ad450f27e552a724c7a40f41c2e
Matt <markricio@gmail.com>
2021-05-09 00:59:56 -0400

    Checkout Forms & Shipping countries added

--- src/components/Products/styles.js
+++ src/components/Products/styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 export default makeStyles((theme) => ({
   toolbar: theme.mixins.toolbar,
   content: {
-    flexGrow: 1,
+    flexGrow: 2,
     backgroundColor: theme.palette.background.default,
     padding: theme.spacing(3),
   },
